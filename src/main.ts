770c326784d01fb36c6da81f287dbf1cab817a89
Reo Tamai <reotamai5@gmail.com>
2023-12-12 20:45:17 -0800

    updated frontend url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200' });
+  app.enableCors({ origin: process.env.FRONTEND_URL });
   await app.listen(3000);
 }
 bootstrap();
